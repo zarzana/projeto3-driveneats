bae5db4534685f933529d385386aa63cf2f6f310
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-03-31 16:45:37 -0300

    Implemented purchase box text replacement

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', init);
 
+var purchaseMessage = ''
+
 function init() {
 
     singleAlternatingSelectionById('dishes', 'product-box', 'product-box-selected')
     singleAlternatingSelectionById('drinks', 'product-box', 'product-box-selected')
     singleAlternatingSelectionById('desserts', 'product-box', 'product-box-selected')
     confirmationButtonListener()
+    confirmCancelListener(purchaseMessage)
 
 }
 
@@ -47,24 +50,59 @@ function confirmationButtonListener() {
 
     confirmationButton = document.getElementsByClassName('confirmation-button-inactive')[0];
     confirmationButton.addEventListener('click', () => {
-        finalPrice = purchaseMessage();
-        console.log(finalPrice)
+        purchaseBoxAndMessage();
+        console.log(purchaseMessage)
+        document.getElementsByClassName('confirmation-menu')[0].classList.remove('hidden');
     })
 
 }
 
-function purchaseMessage() {
+function purchaseBoxAndMessage() {
 
     var selectedProducts = document.querySelectorAll('.product-box-selected');
-    var totalSum = 0.0;
+    var confirmationLines = document.querySelectorAll('.confirmation-line');
+    var totalSum = 0;
+    var confirmationNames = ['- Prato: ', '- Bebida: ', '- Sobremesa: ', 'Total: ']
+    var currentPurchaseMessage = 'Olá, gostaria de fazer o pedido:\n';
 
-    if (selectedProducts.length == 3) {
-        selectedProducts.forEach(product => {
-            totalSum += parseFloat(product.getElementsByClassName('product-price')[0].innerHTML.substring(3).replace(',', '.'));
-        })
-        totalSumString = 'R$ ' + totalSum.toFixed(2).replace('.', ',');
-        return totalSumString;
+    for (let i = 0; i < 4; i++) {
+
+        if (i < 3) {
+            let productName = selectedProducts[i].getElementsByClassName('product-name')[0].innerHTML;
+            let productPrice = selectedProducts[i].getElementsByClassName('product-price')[0].innerHTML.substring(3);
+            totalSum += parseFloat(productPrice.replace(',', '.'));
+            let productInformation = [productName, productPrice];
+            for (let j = 0; j < 2; j++) {
+                confirmationLines[i].children[j].innerHTML = productInformation[j];
+            }
+            // writing message
+            currentPurchaseMessage += confirmationNames[i] + productName + '\n'
+        }
+
+        else {
+            totalSumString = 'R$ ' + totalSum.toFixed(2).replace('.', ',')
+            confirmationLines[i].children[1].innerHTML = '<strong>' + totalSumString + '</strong>';
+            // writing message
+            currentPurchaseMessage += confirmationNames[i] + totalSumString
+        }
+
     }
+
+    purchaseMessage = currentPurchaseMessage
 
 }
 
+function confirmCancelListener(message) {
+
+    var confirmButton = document.getElementById('confirmButton');
+    var cancelButton = document.getElementById('cancelButton');
+
+    confirmButton.addEventListener('click', () => {
+        // send message to WhatsApp Web
+    })
+
+    cancelButton.addEventListener('click', () => {
+        document.getElementsByClassName('confirmation-menu')[0].classList.add('hidden');
+    })
+
+}
